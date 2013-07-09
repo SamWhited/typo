c273096fc6b2f9c9269cdda469d5430f2b89b49e
Sam Whited <sam@samwhited.com>
2013-07-09 19:39:23 -0400

    Use pure CSS for .post-thumbnail hover effect

--- source/js/typo.js
+++ source/js/typo.js
@@ -1,9 +1,4 @@
 (function() {
-  $(".post-box").hover(function(){
-    $(this).find(".post-thumbnail").css("border-radius", "1em");
-  }, function(){
-    $(this).find(".post-thumbnail").css("border-radius", "150px");
-  });
   $(".post-box").click(function(){
     window.location=$(this).find("a").attr("href");
     return false;
